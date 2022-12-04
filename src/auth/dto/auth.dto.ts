351a0cc3ac2925665e45724cca2b4c2f0e762211
Nestor <nacastroc@gmail.com>
2022-12-04 16:46:22 -0500

    Fix debug and add DTO to payload validation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 
 export class AuthDto {
   @IsEmail()
@@ -8,4 +14,22 @@ export class AuthDto {
   @IsString()
   @IsNotEmpty()
   password: string;
+}
+
+export class TokenPayloadDto {
+  @IsInt()
+  @IsNotEmpty()
+  sub: number;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  exp: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  iat: number;
 }
--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { TokenPayloadDto } from '../dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -10,5 +11,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: _configService.get('JWT_SECRET'),
     });
+  }
+
+  validate(payload: TokenPayloadDto) {
+    return payload;
   }
 }
